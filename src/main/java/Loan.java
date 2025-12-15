97c75e143226ce0902e2776aac4008b8f03eebe5
mayboll <igonzalezs@ceinmark.net>
2025-12-15 14:00:48 +0100

    I dont know

--- src/main/java/Loan.java
+++ src/main/java/Loan.java
@@ -1,2 +1,99 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+
 public class Loan {
+    private Book book;
+    private Member member;
+    private LocalDate loanDate;
+    private LocalDate dueDate;
+    private LocalDate returnDate;
+    private boolean active;
+    private double penalty;
+
+    public Loan(Book book, Member member) {
+        this.book = book;
+        this.member = member;
+        this.loanDate = LocalDate.now();
+        this.dueDate = loanDate.plusDays(14);
+        this.returnDate = null;
+        this.active = true;
+        this.penalty = 0;
+    }
+
+    public boolean isPastReturnDate() {
+        return LocalDate.now().isAfter(dueDate);
+    }
+
+    public void computePenalty() {
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public LocalDate getLoanDate() {
+        return loanDate;
+    }
+
+    public void setLoanDate(LocalDate loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public double getPenalty() {
+        return penalty;
+    }
+
+    public void setPenalty(double penalty) {
+        this.penalty = penalty;
+    }
+
+    @Override
+    public String toString() {
+        return "Loan{" +
+                "book=" + book +
+                ", member=" + member +
+                ", loanDate=" + loanDate +
+                ", dueDate=" + dueDate +
+                ", returnDate=" + returnDate +
+                ", active=" + active +
+                ", penalty=" + penalty +
+                '}';
+    }
 }
